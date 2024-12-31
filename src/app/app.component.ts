da8e46c236a2dfed17f5661fc3a4e5ab8ad61048
Sujith M A <sujith.ma@beretail.in>
2024-12-31 13:29:50 +0530

    SCRUM-6: App created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'nby-root',
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Neelayambary';
 }
